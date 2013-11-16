fc158eb22a80b244107cd6323a0f722b153c6f38
Jasal Vadgama <jasal@blacklabelcreative.com>
2013-11-16 18:18:26 +0000

    updating provisions for node

--- provision/lamp.sh
+++ provision/lamp.sh
@@ -1,77 +1,96 @@
 #!/usr/bin/env bash
 
-# if apache2 does no exist
+# node.js settings
+NODE_VERSION=0.10.22
+NODE_SOURCE=http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz
+
+# If apache2 does not exist
 if [ ! -f /etc/apache2/apache2.conf ];
 then
-	echo "Provisioning Vagrant LAMP"
+    echo "INFO: Provisioning Vagrant LAMP"
 
-	# Update apt-get
-	echo "Updating apt-get..."
-	apt-get update
+    # Update apt-get
+    echo "INFO: Updating apt-get..."
+    apt-get update
 
-	# Install MySQL
-	echo "Installing mysql..."
-	echo "mysql-server-5.5 mysql-server/root_password password vagrant" | debconf-set-selections
-	echo "mysql-server-5.5 mysql-server/root_password_again password vagrant" | debconf-set-selections
-	apt-get install -y mysql-server
+    # Install git
+    echo "INFO: Installing git..."
+    apt-get install -y git-core
 
-	# Install apache2
-	echo "Installing apache2..."
-	apt-get install -y apache2
-	rm -rf /var/www
-	ln -fs /vagrant/src /var/www
-	echo "/var/www === /vagrant/src"
+    # Install MySQL
+    echo "INFO: Installing mysql..."
+    echo "mysql-server-5.5 mysql-server/root_password password vagrant" | debconf-set-selections
+    echo "mysql-server-5.5 mysql-server/root_password_again password vagrant" | debconf-set-selections
+    apt-get install -y mysql-server
 
-	# Install PHP5 support
-	echo "Installing php5..."
-	apt-get install -y php5 libapache2-mod-php5 php-apc php5-mysql php5-dev
+    # Install apache2
+    echo "INFO: Installing apache2..."
+    apt-get install -y apache2
+    rm -rf /var/www
+    ln -fs /vagrant/src /var/www
+    echo "/var/www === /vagrant/src"
 
-	# Install OpenSSL
-	echo "Installing OpenSSL..."
-	apt-get install -y openssl
+    # Install PHP5
+    echo "INFO: Installing php5..."
+    apt-get install -y php5 libapache2-mod-php5 php-apc php5-mysql php5-dev
 
-	# If phpmyadmin does not exist
-	if [ ! -f /etc/phpmyadmin/config.inc.php ];
-	then
+    # Install OpenSSL
+    echo "INFO: Installing OpenSSL..."
+    apt-get install -y openssl
 
-		# Used debconf-get-selections to find out what questions will be asked
-		# This command needs debconf-utils
+    # If phpmyadmin does not exist
+    if [ ! -f /etc/phpmyadmin/config.inc.php ];
+    then
+        # Used debconf-get-selections to find out what questions will be asked
+        # This command needs debconf-utils
 
-		# Handy for debugging. clear answers phpmyadmin: echo PURGE | debconf-communicate phpmyadmin
+        # Handy for debugging. clear answers phpmyadmin: echo PURGE | debconf-communicate phpmyadmin
 
-		echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+        echo "INFO: Installing phpmyadmin..."
 
-		echo 'phpmyadmin phpmyadmin/app-password-confirm password vagrant' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/mysql/admin-pass password vagrant' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/password-confirm password vagrant' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/setup-password password vagrant' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/database-type select mysql' | debconf-set-selections
-		echo 'phpmyadmin phpmyadmin/mysql/app-pass password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/dbconfig-install boolean false' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
 
-		echo 'dbconfig-common dbconfig-common/mysql/app-pass password vagrant' | debconf-set-selections
-		echo 'dbconfig-common dbconfig-common/mysql/app-pass password' | debconf-set-selections
-		echo 'dbconfig-common dbconfig-common/password-confirm password vagrant' | debconf-set-selections
-		echo 'dbconfig-common dbconfig-common/app-password-confirm password vagrant' | debconf-set-selections
-		echo 'dbconfig-common dbconfig-common/app-password-confirm password vagrant' | debconf-set-selections
-		echo 'dbconfig-common dbconfig-common/password-confirm password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/app-password-confirm password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/mysql/admin-pass password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/password-confirm password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/setup-password password vagrant' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/database-type select mysql' | debconf-set-selections
+        echo 'phpmyadmin phpmyadmin/mysql/app-pass password vagrant' | debconf-set-selections
 
-		apt-get install -y phpmyadmin
-	fi
+        echo 'dbconfig-common dbconfig-common/mysql/app-pass password vagrant' | debconf-set-selections
+        echo 'dbconfig-common dbconfig-common/mysql/app-pass password' | debconf-set-selections
+        echo 'dbconfig-common dbconfig-common/password-confirm password vagrant' | debconf-set-selections
+        echo 'dbconfig-common dbconfig-common/app-password-confirm password vagrant' | debconf-set-selections
+        echo 'dbconfig-common dbconfig-common/app-password-confirm password vagrant' | debconf-set-selections
+        echo 'dbconfig-common dbconfig-common/password-confirm password vagrant' | debconf-set-selections
 
-	# Enable mod_rewrite
-	echo "Enabling mod_rewrite"
-	a2enmod rewrite
+        apt-get install -y phpmyadmin
+    fi
 
-	# Enable SSL
-	echo "Enabling SSL"
-	a2enmod ssl
+    # Enable mod_rewrite
+    echo "INFO: Enabling mod_rewrite..."
+    a2enmod rewrite
 
-	# Restart services
-	/etc/init.d/apache2 restart
+    # Enable SSL
+    echo "INFO: Enabling SSL..."
+    a2enmod ssl
 
-	# Clean up
-	apt-get clean
+    # Install node.js
+    echo "INFO: Installing node.js $NODE_VERSION..."
+    apt-get update
+    apt-get install -y python-software-properties python g++ make
+    add-apt-repository -y ppa:chris-lea/node.js
+    apt-get update
+    apt-get install -y nodejs
 
-	echo "All done, now go code something"
+    # Restart services
+    echo "INFO: Restarting apache..."
+    /etc/init.d/apache2 restart
+
+    # Clean up
+    echo "INFO: Cleaning up..."
+    apt-get clean
+
+    echo "INFO: Provisioning Vagrant LAMP complete!"
 fi
--- provision/node.sh
+++ provision/node.sh
@@ -1,34 +1,44 @@
 #!/usr/bin/env bash
 
 # node settings
-NODE_VERSION=0.10.10
+NODE_VERSION=0.10.
 NODE_SOURCE=http://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz
 
 # if node.js does not exist
 if [ ! -e /opt/node/$NODE_VERSION ];
 then
-	echo "Provisioning Vagrant Node.js"
+    echo "INFO: Provisioning Vagrant node.js"
 
-	# Update apt-get
-	echo "Updating apt-get..."
-	apt-get update
+    # Update apt-get
+    echo "INFO: Updating apt-get..."
+    apt-get update
 
-	# install node.js
-	echo "Installing Node.js $NODE_VERSION..."
-	cd /usr/src
-	wget --quiet $NODE_SOURCE
-	tar xf node-v$NODE_VERSION.tar.gz
-	cd node-v$NODE_VERSION
+    # Install git
+    echo "INFO: Installing git..."
+    apt-get install -y git-core
 
-	# configure
-	./configure --prefix=/opt/node/$NODE_VERSION
+    # Install node.js
+    # Install node.js
+    echo "INFO: Installing node.js $NODE_VERSION..."
+    apt-get update
+    apt-get install -y python-software-properties python g++ make
+    add-apt-repository -y ppa:chris-lea/node.js
+    apt-get update
+    apt-get install -y nodejs
 
-	# make and install
-	make
-	make install
+    # Install mongodb
+    echo "INFO: Installing mongodb..."
+    apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+    echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list
+    apt-get -y update
+    apt-get -y install mongodb-10gen
+    # Set up the required directories and permissions
+    mkdir -p /data/db
+    chown mongodb /data/db
 
-	# Clean up
-	apt-get clean
+    # Clean up
+    echo "INFO: Cleaning up..."
+    apt-get clean
 
-	echo "All done, now go code something"
+    echo "INFO: Provisioning Vagrant node.js complete!"
 fi
